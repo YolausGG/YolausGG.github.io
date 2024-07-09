5cfb8a4d086a1f2cffab5bbc237323bc5926d694
Yolaus Godoy <81392395+YolausGG@users.noreply.github.com>
2024-07-09 10:20:54 -0300

    portafolios a .io

---
+++ proyects/PotafoliosWeb/javascript/imagenesProyectos.js
@@ -0,0 +1,64 @@
+let imgsCarruselGDB = [
+    "/img/HerramientasGDB.webp",
+    "/img/fichaBovino.webp"
+]
+let imgsCarruselEcommerce = [
+    "/img/HerramientasEcommerce.webp"
+]
+let imgsCarruselProyectosDuranteLaCarrera = [
+    "/img/HerramientasProyectosDutanteLaCarrera.webp",
+    "/img/imgMutualistaSF.webp",
+    "/img/fichaBovino.webp"
+]
+let imgsCarruselPortafolioWeb = [
+    "/img/HerramientasPortafolioWeb.webp"
+]
+
+cargarCarrusel = (nameProject) => {
+
+    let img = document.getElementById(`idImgCarrusel${nameProject}`)
+    let atras = document.getElementById(`atras${nameProject}`)
+    let adelante = document.getElementById(`adelante${nameProject}`)
+    let actual = 0;
+
+    console.log(nameProject);
+
+    let selectImgsCarrusel = []
+    
+    switch (nameProject) {
+        case "GDB":
+            selectImgsCarrusel = imgsCarruselGDB
+            break;
+        case "ECommerce":
+            selectImgsCarrusel = imgsCarruselEcommerce
+            break;
+        case "ProyectosDuranteLaCarrera":
+            selectImgsCarrusel = imgsCarruselProyectosDuranteLaCarrera
+            break;
+        case "PortafolioWeb":
+            selectImgsCarrusel = imgsCarruselPortafolioWeb
+            break;
+        default:
+            break;
+    }
+
+    atras.addEventListener('click', function () {
+        actual -= 1
+
+        if (actual == -1) {
+            actual = selectImgsCarrusel.length - 1
+        }
+
+        img.innerHTML = `<img class="imgCarrusel" src="${selectImgsCarrusel[actual]}" alt="Imagen del Carrusel" loading="lazy">`
+    })
+
+    adelante.addEventListener('click', function () {
+        actual += 1
+
+        if (actual == selectImgsCarrusel.length) {
+            actual = 0
+        }
+
+        img.innerHTML = `<img class="imgCarrusel" src="${selectImgsCarrusel[actual]}" alt="Imagen del Carrusel" loading="lazy">`
+    })
+}
---
+++ proyects/PotafoliosWeb/javascript/index.js
@@ -0,0 +1,53 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load("particles-js", "particlesjs-config.json", function () { });
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropDownMenu = document.querySelector(".dropdown_nav-menu");
+
+toggleBtn.addEventListener("click", () => {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
+
+  toggleBtnIcon.classList = isOpen ? "fi fi-br-cross" : "fi fi-br-menu-burger";
+});
+
+const btnsCerrarInfo = document.getElementsByClassName("btnCerrarInfo");
+const btnsAbrirInfo = document.getElementsByClassName("btnAbrirInfo");
+const dialogsInfo = document.querySelectorAll("dialog");
+
+Array.prototype.forEach.call(btnsAbrirInfo, (btnAbrirInfo) => {
+  btnAbrirInfo.addEventListener("click", () => {
+    dialogsInfo.forEach((dialgoInfo) => {
+      if (btnAbrirInfo.id == "btnGDB" && dialgoInfo.id == "dialogGDB") {
+        dialgoInfo.showModal();
+      }
+      if (
+        btnAbrirInfo.id == "btnECommerce" &&
+        dialgoInfo.id == "dialogEcommerce"
+      ) {
+        dialgoInfo.showModal();
+      }
+      if (
+        btnAbrirInfo.id == "btnProyectosDuranteLaCarrera" &&
+        dialgoInfo.id == "dialogProyectosDuranteLaCarrera"
+      ) {
+        dialgoInfo.showModal();
+      }
+      if (
+        btnAbrirInfo.id == "btnPortafolioWeb" &&
+        dialgoInfo.id == "dialogPortafolioWeb"
+      ) {
+        dialgoInfo.showModal();
+      }
+    });
+  });
+});
+
+Array.prototype.forEach.call(btnsCerrarInfo, (btnCerrarInfo) => {
+  btnCerrarInfo.addEventListener("click", () => {
+    dialogsInfo.forEach((dialgoInfo) => {
+      dialgoInfo.close();
+    });
+  });
+});
